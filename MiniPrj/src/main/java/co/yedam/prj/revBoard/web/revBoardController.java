5beb8b4a03c6a5cd550463e3090ced33d6de0095
admin <admin@YD02-14>
2021-06-11 10:01:31 +0900

    21.06.11 10:11 paging 제작 중

--- MiniPrj/src/main/java/co/yedam/prj/revBoard/web/revBoardController.java
+++ MiniPrj/src/main/java/co/yedam/prj/revBoard/web/revBoardController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import co.yedam.prj.revBoard.service.RevBoardService;
-import co.yedam.prj.revBoard.vo.RevBoardVO;
+import co.yedam.prj.revBoard.service.revBoardService;
+import co.yedam.prj.revBoard.vo.revBoardVO;
 
 @Controller
-public class RevBoardController {
+public class revBoardController {
 	
 	@Autowired
-	private RevBoardService dao;
+	private revBoardService dao;
 	
 	@RequestMapping("/revBoardList.do")
 	public String revBoardList(Model model) {
@@ -29,7 +29,7 @@ public class RevBoardController {
 	}
 	
 	@RequestMapping("/revBoardListPaging.do")
-	public String revBoardListPaging(Model model, RevBoardVO vo,@RequestParam(value = "start", required = false, defaultValue="1") int start) {
+	public String revBoardListPaging(Model model, revBoardVO vo,@RequestParam(value = "start", required = false, defaultValue="1") int start) {
 		
 		vo.setFirstRecordIndex(1 + (start-1)*10);
 		vo.setLastRecordIndex(10*start);
