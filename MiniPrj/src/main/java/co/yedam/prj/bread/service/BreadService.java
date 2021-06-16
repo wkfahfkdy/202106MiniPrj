ef07b768f7d9654c2d490f7d5a565e46ec6d2c5b
admin <admin@YD02-03>
2021-06-16 12:02:42 +0900

    commit

--- MiniPrj/src/main/java/co/yedam/prj/bread/service/BreadService.java
+++ MiniPrj/src/main/java/co/yedam/prj/bread/service/BreadService.java
@@ -23,12 +23,12 @@ public interface BreadService {
 	public int storeDelete(BreadVO vo); // 스토어 삭제
 	public int storeUpdate(BreadVO vo); // 스토어 수정
 	
-<<<<<<< HEAD
+
 	public BreadVO storeSelectAll(BreadVO vo);
-=======
+
 	
 	public BreadVO storeAdr(BreadVO vo);	// 스토어 주소 가져오기
->>>>>>> branch 'master' of https://github.com/wkfahfkdy/202106MiniPrj.git
+
 	public BreadVO storeSelect(BreadVO vo); //스토어 셀렉트(빵 페이지 하단 스토어)
 	public List<BreadVO> storeTopThree();
 	
